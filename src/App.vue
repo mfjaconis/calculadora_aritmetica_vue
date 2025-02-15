f9068cb05976959e4e301d369b93e0ed633d0671
mfjaconis <matheusjaconis@gmail.com>
2025-02-15 10:09:13 -0300

    iniciando o projeto da calculadora aritmética

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<template>
+    <header class="bg-secondary p-5 mt-5 container text-center">
+        <h1>Calculadora Aritmética</h1>
+        <p>Faça o seu cálculo através da nossa calculadora aritmética</p>
+    </header>
+</template>
